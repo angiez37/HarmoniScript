43ef36de7016292d1d5a6bb6e7e0068f4f2bacae
Angel Saka <angelsaka@yahoo.com>
2024-08-16 23:07:20 -0400

    explore  me page done

---
+++ src/components/Explore.jsx
@@ -0,0 +1,17 @@
+
+const Explore = () => {
+  return (
+    <div className="flex justify-center items-center m-5">
+        <div className="flex justify-center items-center border shadow-secondary border-10 border-secondary shadow-2xl m-20 p-10">
+            <div className="flex flex-col justify-center items-center m-20 gap-5">
+                <p className="font-serif text-2xl text-black text-center leading-8">Meet <span className="font-bold">HarmoniScript!</span></p>
+                <p className="font-serif text-2xl text-black text-center leading-8">A free website where you can upload you favorite songs to be translated to sheet music in a matter seconds.</p>
+                <p className="font-serif text-2xl text-black text-center leading-8">All you need to do is choose your song from your device, select your instrument and click upload. It's that simple!</p>
+                <p className="font-serif text-2xl text-black text-center leading-8">Start exploring with <span className="font-bold">HarmoniScript!</span></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Explore
