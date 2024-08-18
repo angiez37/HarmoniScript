438a16849dacc74b952a34994d65320218088803
Angel Saka <angelsaka@yahoo.com>
2024-08-18 01:42:40 -0400

    script deleted

--- server/script.js
+++
@@ -1,8 +0,0 @@
-const video = document.getElementById("video")
-formData.append("video", video.files[0])
-
-//update the url as follows
-fetch("http://localhost:5000/upload", {
-    method: "POST",
-    body: formData,
-})
